36f633c1f6487f9c49f901dda9455ff174c0dda9
batmhannJ <reyeshannahjoy82@gmail.com>
2026-05-11 18:10:11 +0800

    Refactor code structure for improved readability and maintainability

---
+++ flutter_app/android/app/src/main/kotlin/com/telexph/teamsclone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.telexph.teamsclone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,24 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import emoji_picker_flutter
+import file_selector_macos
+import flutter_secure_storage_macos
+import screen_retriever
+import shared_preferences_foundation
+import sqflite_darwin
+import window_manager
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
+}
